be1498a04af7950f84dbe39e253cba50e6fe5555
Drew DeVault <sir@cmpwn.com>
2021-08-04 13:59:49 +0200

    temporary tests: flesh out abort
    Signed-off-by: Drew DeVault <sir@cmpwn.com>

--- tests/rt.c
+++ tests/rt.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <unistd.h>
 
 void *c_memcpy(void *dest, const void *src, size_t n) {
 	unsigned char *a = dest;
@@ -7,4 +8,15 @@ void *c_memcpy(void *dest, const void *src, size_t n) {
 		a[i] = b[i];
 	}
 	return dest;
+}
+
+struct ha_str {
+	size_t len, cap;
+	char *str;
+};
+
+void c_abort(struct ha_str str) {
+	write(2, str.str, str.len);
+	write(2, "\n", 1);
+	abort();
 }
