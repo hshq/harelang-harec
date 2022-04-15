9a3c054915fd941de324a94cdd60325eccdae561
Karl Schultheisz <k@kdsch.org>
2022-04-15 14:53:38 -0400

    types: use promotion algo to cast flexible consts
    Fix a segfault in the type checker for incorrect code such as
    
    	let a: [0xf1a97acc: [0]int]int;
    
    for which Valgrind amusingly reports
    
    	Address 0xf1a97acc is not stack'd, malloc'd or (recently) free'd
    
    by using the promotion algorithm to determine castability of
    flexible constants.
    
    See spec commit be14631dfb548a1eebc871eb55e7aca4d9f4f7a0,
    sections 6.6.26.7 and 6.10.
    
    Signed-off-by: Karl Schultheisz <k@kdsch.org>

--- src/types.c
+++ src/types.c
@@ -918,7 +918,7 @@ type_is_castable(const struct type *to, const struct type *from)
 	case STORAGE_FCONST:
 	case STORAGE_ICONST:
 	case STORAGE_RCONST:
-		return lower_const(from_orig, to_orig);
+		return promote_const(from_orig, to_orig);
 	case STORAGE_I8:
 	case STORAGE_I16:
 	case STORAGE_I32:
