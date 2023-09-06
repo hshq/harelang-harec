ba655666da87f32ab9e9c875e7f03f6d8c89e445
Ember Sawady <ecs@d2evs.net>
2023-09-06 23:17:40 +0000

    alpha-sort usage
    Signed-off-by: Ember Sawady <ecs@d2evs.net>

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ static void
 usage(const char *argv_0)
 {
 	xfprintf(stderr,
-		"Usage: %s [-v] [-a arch] [-D ident[:type]=value] [-o output] [-T] [-t typedefs] [-N namespace] input.ha...\n",
+		"Usage: %s [-a arch] [-D ident[:type]=value] [-N namespace] [-o output] [-T] [-t typedefs] [-v] input.ha...\n",
 		argv_0);
 }
 
@@ -65,7 +65,7 @@ main(int argc, char *argv[])
 	struct ast_global_decl *defines = NULL, **next_def = &defines;
 
 	int c;
-	while ((c = getopt(argc, argv, "a:D:ho:Tt:N:v")) != -1) {
+	while ((c = getopt(argc, argv, "a:D:hN:o:Tt:v")) != -1) {
 		switch (c) {
 		case 'a':
 			target = optarg;
@@ -74,15 +74,9 @@ main(int argc, char *argv[])
 			*next_def = parse_define(argv[0], optarg);
 			next_def = &(*next_def)->next;
 			break;
-		case 'o':
-			output = optarg;
-			break;
-		case 'T':
-			is_test = true;
-			break;
-		case 't':
-			typedefs = optarg;
-			break;
+		case 'h':
+			usage(argv[0]);
+			return EXIT_SUCCESS;
 		case 'N':
 			unit.ns = xcalloc(1, sizeof(struct identifier));
 			if (strlen(optarg) == 0) {
@@ -97,11 +91,17 @@ main(int argc, char *argv[])
 				lex_finish(&lexer);
 			}
 			break;
+		case 'o':
+			output = optarg;
+			break;
+		case 'T':
+			is_test = true;
+			break;
+		case 't':
+			typedefs = optarg;
+			break;
 		case 'v':
 			printf("harec %s\n", VERSION);
-			return EXIT_SUCCESS;
-		case 'h':
-			usage(argv[0]);
 			return EXIT_SUCCESS;
 		default:
 			usage(argv[0]);
