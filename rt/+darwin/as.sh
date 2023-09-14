68509f3665f9152187633c8ff4ba7852021aa062
hshq <hshqcn@qq.com>
2023-09-15 02:43:40 +0800

    merge

--- rt/+darwin/as.sh
+++ rt/+darwin/as.sh
@@ -1,31 +1,62 @@
 #!/bin/sh
 
-arch=$(uname -m)
-flags=-g
+AS=/usr/bin/as
+
+function fix_asm() {
+sed -r \
+    -e 's/^\.section "\.text([^"]*)"(,"([^"]*))?"$/.text # \1, \3/g' \
+    -e 's/^\.section "\.data([^"]*)"$/.data # \1/g' \
+    -e 's/^\.section "\.bss([^"]*)"$/.bss # \1/g' \
+    \
+    -e 's/^\.section "\.init_array"(,"([^"]*)")?$/.section __DATA, .init_array # \2/g' \
+    -e 's/^\.section "\.fini_array"(,"([^"]*)")?$/.section __DATA, .fini_array # \2/g' \
+    -e 's/^\.section "\.test_array"(,"([^"]*)")?$/.section __DATA, .test_array # \2/g' \
+    \
+    -e 's/^\.section \.abort "([^"]+)"$/.section __DATA, .abort # \1/g' \
+    \
+    -e 's/^(\.type .+)$/# \1/g' \
+    -e 's/^(\.global) (rt\..+)$/\1 _\2/g' \
+    -e 's/^(rt\..+:)$/_\1/g' \
+    \
+    $1
+}
+
+function header() {
+    echo
+    echo
+    echo "# ----------------------------------------------------------------"
+    echo ".file \"$1\""
+    echo "# ----------------------------------------------------------------"
+    echo
+}
+
 
 args=
-path=
-until [ $# -eq 1 ]; do
-	args="$args $1"
-	shift
+code=
+until [ $# -eq 0 ]; do
+    case $1 in
+        -o)
+            args="$args $1 $2"
+            shift
+            ;;
+        -g)
+            args="$args $1"
+            ;;
+        -*)
+            echo "Unsupported options: $*"
+            exit 1
+            ;;
+        *)
+            # file="$(fix_asm $1)"
+
+            # # IFS=$'\n'
+            file=$(cat "$1")
+
+            code="$code$(header $1)\n$file"
+            ;;
+    esac
+    shift
 done
-path=$1
 
-dir=".*\/rt\/\+$arch$"
-if [[ $(dirname $path) =~ $dir ]]
-then
-	case $(basename -s .s $path) in
-		cpuid_native|fenv|getfp|longjmp|restore|setjmp)
-			sed -r \
-    			-e 's/^(.section ".text.*")$/.text # \1/g' \
-				-e 's/^([ \t]*)(.type[ \t]+.+)$/\1# \2/g' \
-				-e 's/^([ \t]*.global[ \t]+)(rt\..+)$/\1_\2/g' \
-				-e 's/^([ \t]*)(rt\..+:)$/\1_\2/g' \
-				$path | /usr/bin/as $flags $args --
-			exit 0
-			;;
-	esac
-fi
-
-/usr/bin/as $flags $args $path
-exit 0
+# echo "$code" | $AS $args --
+echo "$code" | fix_asm | $AS $args --
