ba1565ce5eaeaf45b5ca2a3656bca99298fdf3af
hshq <hshqcn@qq.com>
2024-03-15 07:36:54 +0800

    Optimize the compilation process

--- configs/darwin.mk
+++ configs/darwin.mk
@@ -4,25 +4,30 @@ BINDIR = $(PREFIX)/bin
 
 # variables used during build
 PLATFORM = darwin
+# ARCH = $(shell source "./rt/+darwin/arch.sh" arch)
+# ARCH = $(shell hare-build.sh arch)
 ARCH = $(shell hare-arch.sh)
 HARECFLAGS =
 QBEFLAGS =
 ASFLAGS =
 # LDLINKFLAGS = --gc-sections -z noexecstack
-LDLINKFLAGS = -dead_strip
 CFLAGS = -g -std=c11 -D_XOPEN_SOURCE=700 -Iinclude \
 	-Wall -Wextra -Werror -pedantic -Wno-unused-parameter
-LDFLAGS = -dead_strip
+LDFLAGS =
 LIBS = -lm
 
 # commands used by the build script
+# CC = hare-cc.sh
 CC = cc
-AS = hare-build.sh -as
-LD = hare-build.sh ld
+# AS = hare-build.sh as
+AS = hare-as.sh
+# LD = hare-build.sh -ld
+LD =  hare-ld.sh
+# QBE = hare-qbe.sh
+# QBE = hare-build.sh --qbe
 QBE = qbe
 
 # build locations
-# HARECACHE = .cache
 HARECACHE = /Volumes/hare-cache/harec.cache
 BINOUT = .bin
 
